66ebc77a5dc7f6eb6922847501abbfc344ae79c7
David Poole <14233215+davidlpoole@users.noreply.github.com>
2023-08-04 19:01:12 +0100

    Update transfer script to allow sending tokens to a list of recipients

--- scripts/transfer.js
+++ scripts/transfer.js
@@ -1,58 +1,94 @@
 const hre = require("hardhat");
 
 function parsePoo(_amount, unitsFrom, _decimals) {
-  const amount = BigInt(_amount);
-  const power10decimals = BigInt(10) ** _decimals;
+    //ensure parameters are all BigInt
+    const amount = BigInt(_amount);
+    const power10decimals = BigInt(10) ** _decimals;
 
-  switch (unitsFrom.toUpperCase()) {
-    case "POO":
-      return amount * power10decimals
-      break
-    case "WEI":
-      return amount / power10decimals
-      break
-    default:
-      return amount;
-  }
+    // convert
+    switch (unitsFrom.toUpperCase()) {
+        case "POO":
+            return amount * power10decimals
+            break
+        case "WEI":
+            return amount / power10decimals
+            break
+        default:
+            return amount;
+    }
 }
 
-async function printDetails(signer, contract) {
-  const address = signer.address;
-  const tokenSymbol = await contract.symbol();
-  const tokenDecimals = await contract.decimals();
-  const balance = await contract.balanceOf(address);
-  const tokenBalance = parsePoo(balance, "wei", tokenDecimals);
-  console.log(`${address} has ${tokenBalance} ${tokenSymbol}`);
+async function printDetails(signerOrAddress, contract) {
+    // get address from either the signer object or address string
+    let address;
+    if (typeof signerOrAddress == "string") {
+        address = signerOrAddress;          // its already an address string
+    } else {
+        address = signerOrAddress.address;  // its a signer object with an address property
+    }
+
+    // get the token details and balance
+    const tokenSymbol = await contract.symbol();
+    const tokenDecimals = await contract.decimals();
+    const balance = await contract.balanceOf(address);
+
+    // convert from wei to token
+    const tokenBalance = parsePoo(balance, "wei", tokenDecimals);
+
+    console.log(`${address} has ${tokenBalance} ${tokenSymbol}`);
 }
 
-async function transfer(from, to, tokenAmount, contract) {
-  const tokenDecimals = await contract.decimals();
-  const tokenSymbol = await contract.symbol();
-  console.log(`sending ${tokenAmount} ${tokenSymbol} from ${from.address} to ${to.address}`);
-  const parsedAmount = parsePoo(tokenAmount.toString(), "POO", tokenDecimals);
-  const tx = await contract.connect(from).transfer(to.address, parsedAmount);
-  return tx.wait();
+async function transfer(signer, to_address, tokenAmount, contract) {
+    // output the current balance of the recipient
+    await printDetails(to_address, contract);
+
+    // get the token symbol and decimal place
+    const tokenDecimals = await contract.decimals();
+    const tokenSymbol = await contract.symbol();
+    // display what transfer is going to happen
+    console.log(`sending ${tokenAmount} ${tokenSymbol} from ${signer.address} to ${to_address}`);
+
+    // convert the token amount to 'wei'
+    const parsedAmount = parsePoo(tokenAmount.toString(), "POO", tokenDecimals);
+    // perform the transfer
+    const tx = await contract.connect(signer).transfer(to_address, parsedAmount);
+    // wait for mining
+    await tx.wait();
+
+    // display recipient balance after transfer
+    await printDetails(to_address, contract);
 }
 
 async function main() {
+    // get the signer wallet/s from hardhat config
+    const [deployer, other] = await ethers.getSigners();
 
-  const [deployer, other] = await ethers.getSigners();
+    // get the details of the deployed token contract
+    const deployedContract = '0x242DEf03BF69103efA66Fe8933b8fcD6c45C1C85';
+    const contract = await ethers.getContractAt('Pooley', deployedContract);
 
-  const deployedContract = '0x242DEf03BF69103efA66Fe8933b8fcD6c45C1C85';
-  const contract = await ethers.getContractAt('Pooley', deployedContract);
+    // display the sender's balance before transactions
+    await printDetails(deployer, contract);
 
-  await printDetails(deployer, contract);
-  await printDetails(other, contract);
+    // a list of addresses to send tokens to
+    const recipient_list = [
+        other.address,
+        other.address,
+        other.address,
+    ];
 
-  await transfer(other, deployer, 1, contract);
+    // iterate over the list of recipients, and send an amount of the token to each
+    for (const recipient of recipient_list) {
+        await transfer(deployer, recipient, 1, contract);
+    }
 
-  await printDetails(deployer, contract);
-  await printDetails(other, contract);
+    // display the senders final balance
+    await printDetails(deployer, contract);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+    console.error(error);
+    process.exitCode = 1;
 });
